1afeb7bdbbec9e4e0536487ca04329763450eab2
klauha <claudiaalvaro17@gmail.com>
2024-03-23 11:02:14 +0100

    feat= add inouts to login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+
+import React, { useState } from 'react'
+import { Input } from '../../common/Input/input'
 import "./Login.css"
 
 export const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const LogMe = () => {
+    console.log(email);
+    console.log(password);
+  }
+
+  const inputHandler = (e) => {
+    if (e.target.name === "email") {
+        setEmail(e.target.value)
+    }
+    if (e.target.name === "password") {
+        setPassword(e.target.value)
+    }
+
+  }
   return (
-    <div className='loginDesign'>Login</div>
+    <div className='loginDesign'>
+            <Input
+                type="email"
+                placeHolder="email"
+                name="email"
+                onChangeFunction={(e) => inputHandler(e)}
+            />
+            <Input
+                type="password"
+                placeHolder="password"
+                name="password"
+                onChangeFunction={(e) => inputHandler(e)}
+            />
+
+
+            <button onClick={LogMe}>Login</button>
+        
+    </div>
   )
 }
