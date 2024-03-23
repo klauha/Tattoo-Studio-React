9bddcc818bc0824a2aa1ce9631428487afd9dda3
klauha <claudiaalvaro17@gmail.com>
2024-03-23 12:19:41 +0100

    feat: add fetch Post to login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,41 +4,55 @@ import { Input } from '../../common/Input/input'
 import "./Login.css"
 
 export const Login = () => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
 
-  const LogMe = () => {
-    console.log(email);
-    console.log(password);
-  }
-
-  const inputHandler = (e) => {
-    if (e.target.name === "email") {
-        setEmail(e.target.value)
+  const [bodyCredentials, setBodyCredentials] = useState(
+    {
+      email: "",
+      password: ""
     }
-    if (e.target.name === "password") {
-        setPassword(e.target.value)
-    }
-
-  }
-  return (
-    <div className='loginDesign'>
-            <Input
-                type="email"
-                placeHolder="email"
-                name="email"
-                onChangeFunction={(e) => inputHandler(e)}
-            />
-            <Input
-                type="password"
-                placeHolder="password"
-                name="password"
-                onChangeFunction={(e) => inputHandler(e)}
-            />
-
-
-            <button onClick={LogMe}>Login</button>
-        
-    </div>
   )
+
+  const LogMe = async() => {
+    const response = await fetch(
+      'http://localhost:4000/api/auth/login',
+      {
+        method: "Post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bodyCredentials),
+      }
+   )
+   const loggedUser = await response.json
+  }
+
+const inputHandler = (e) => {
+  setBodyCredentials((prevState) => (
+    {
+      ...prevState,
+      [e.target.name]: e.target.value
+    }
+  ))
+
+}
+return (
+  <div className='loginDesign'>
+    <Input
+      type="email"
+      placeHolder="email"
+      name="email"
+      onChangeFunction={(e) => inputHandler(e)}
+    />
+    <Input
+      type="password"
+      placeHolder="password"
+      name="password"
+      onChangeFunction={(e) => inputHandler(e)}
+    />
+
+
+    <button onClick={LogMe}>Login</button>
+
+  </div>
+)
 }
