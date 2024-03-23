987ab464ccc25e8d0e8c7bf650f3a7eec4ba89d1
klauha <claudiaalvaro17@gmail.com>
2024-03-23 11:20:41 +0100

    feat: refactor register to credentials literal object

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -4,48 +4,43 @@ import { Input } from '../../common/Input/input'
 import './Register.css'
 
 export const Register = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [first_name, setfirst_Name] = useState("")
-    const [last_name, setlast_Name] = useState("")
+    // const [email, setEmail] = useState("")
+    // const [password, setPassword] = useState("")
+
+    const [bodyCredentials, setBodyCredentials] = useState(
+        {
+            email: "",
+            password: ""
+        }
+    )
 
 
     const registerMe = () => {
-        console.log(first_name);
-        console.log(last_name);
-        console.log(email);
-        console.log(password);
+        // console.log(email);
+        // console.log(password);
+        console.log(bodyCredentials.password);
+        console.log(bodyCredentials.email);
 
+        console.log(bodyCredentials);
     }
-    const inputHandler = (e) => {
-        if (e.target.name === "email") {
-            setEmail(e.target.value)
-        }
-        if (e.target.name === "password") {
-            setPassword(e.target.value)
-        }
 
-        if (e.target.name === "first_name") {
-            setfirst_Name(e.target.value)
-        }
-        if (e.target.name === "last_name") {
-            setlast_Name(e.target.value)
-        }
+    const inputHandler = (e) => {
+        // if (e.target.name === "email") {
+        //     setEmail(e.target.value)
+        // }
+        // if (e.target.name === "password") {
+        //     setPassword(e.target.value)
+        // }
+
+        setBodyCredentials( (prevState) => (
+            {
+                ...prevState,
+                [e.target.name]: e.target.value,
+            }
+        ))
     }
     return (
         <div className='registerDesign'>
-            <Input
-                type="text"
-                placeHolder="Nombre"
-                name="last_name"
-                onChangeFunction={(e) => inputHandler(e)}
-            />
-            <Input
-                type="text"
-                placeHolder="Apellidos"
-                name="first_name"
-                onChangeFunction={(e) => inputHandler(e)}
-            />
             <Input
                 type="email"
                 placeHolder="email"
