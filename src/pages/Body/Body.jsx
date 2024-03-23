4848db25057da2bff0d3a40e5d80430c83d2551f
klauha <claudiaalvaro17@gmail.com>
2024-03-23 13:55:04 +0100

    feat: created view home private after to login

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -3,15 +3,17 @@ import { Home } from "../Home/Home"
 import { Login } from "../Login/Login"
 import { Register } from "../Register/Register"
 import { Artists } from "../Artists/Artists"
+import { PrivateHome } from "../PrivateHome/PrivateHome"
 
 
 
 export const Body = () => {
     return (
         <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<PrivateHome />} />
             <Route path="/artists" element={<Artists />} />
             
         </Routes>
---
+++ src/pages/PrivateHome/PrivateHome.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./PrivateHome.css"
+
+export const PrivateHome = () => {
+  return (
+    <div className='privateHomeDesign'>PrivateHome</div>
+  )
+}
