c3e9e2d6cbf56d40a9f3c886d674040b7f82af2b
klauha <claudiaalvaro17@gmail.com>
2024-03-26 16:50:20 +0100

    feat:add react data table to My appointments

---
+++ src/pages/CreateAppointment/CreateAppointment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./CreateAppointment.css"
+
+export const CreateAppointment = () => {
+
+
+  return (
+
+    <div className='createAppoinmentDesign'>CreateAppointments</div>
+  )
+}
--- src/pages/MyAppointments/MyAppointments.jsx
+++ src/pages/MyAppointments/MyAppointments.jsx
@@ -1,18 +1,39 @@
 import { getMyAppointments } from "../../services/apiCalls"
 import "./MyAppointments.css"
+import DataTable from "react-data-table-component"
+
 
 import React, { useEffect, useState } from 'react'
 
 export const MyAppointments = () => {
+  const columns = [
+    {
+      name: "Número de cita",
+      selector: (row,index) => index+1,
+      
+    },
+    {
+      name: "Servicio contratado",
+      selector: row => row.service?.serviceName
 
-  const [userAppointments, setUserAppointments] = useState([{}])  
+    },
+    {
+      name: "Fecha",
+      selector: row => {
+        const date = new Date(row.appointmentDate);
+        return date.toLocaleDateString('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric' });
+      }
+    }
+  ]
+  const [userAppointments, setUserAppointments] = useState([{}])
+
   useEffect(() => {
     const getUserAppointments = async () => {
       const MyAppointments = await getMyAppointments()
-
+      console.table(MyAppointments.data)
       console.table(MyAppointments.data[0].service);
       setUserAppointments(
-
+        MyAppointments.data
       )
     }
     getUserAppointments()
@@ -23,6 +44,18 @@ export const MyAppointments = () => {
 
 
   return (
-    <div className="myAppointmentsDesign">MyAppointments</div>
+    <div className="myAppointmentsDesign">
+
+      <DataTable
+      title="Mis citas"
+        columns={columns}
+        data={userAppointments}
+        selectableRows
+        onSelectedRowsChange={data =>console.log(data)}
+        log
+      />
+
+
+    </div>
   )
 }
